84e3f4c015807636764d141b192ed73bf4fef878
admin <11@.com>
2019-04-17 09:26:57 +0800

    1

--- app/src/main/java/com/example/admin/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/admin/myapplication/Main2Activity.java
@@ -3,7 +3,9 @@ package com.example.admin.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -26,40 +28,48 @@ public class Main2Activity extends AppCompatActivity {
 
     private TextView textView;
     private Button button;
-    private String[] data={"北京","浙江","安微"};
+    private int[] pids=new int[ ]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    private String[] data={"北京","浙江","安微","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""};
     private ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        this.textView=(TextView) findViewById(R.id.textView);
-        this.button=(Button) findViewById(R.id.button);
-        this.listview=(ListView)findViewById(R.id.listview);
+        this.textView = (TextView) findViewById(R.id.textView);
+        this.button = (Button) findViewById(R.id.button);
+        this.listview = (ListView) findViewById(R.id.listview);
 
-        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
         listview.setAdapter(adapter);
 
+       this.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               Log.v("点击了哪一个",""+position+":"+Main2Activity.this.pids[position]+":"+Main2Activity.this.data[position]);
+           }
+       });
 
-        this.button.setOnClickListener(new View.OnClickListener(){
+        this.button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent intent=new Intent(Main2Activity.this,Main3Activity.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
                 startActivity(intent);
             }
         });
 
         String weatherUrl = "http://guolin.tech/api/china";
-        HttpUtil.sendOkHttpRequest(weatherUrl,new Callback(){
+        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+
             @Override
             public void onFailure(Call call, IOException e) {
 
             }
 
             @Override
-            public void onResponse(Call call, Response response) throws IOException{
+            public void onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
-                String[] result=parseJSONObject(responseText);
-                Main2Activity.this.data=result;
+                String[] result = parseJSONObject(responseText);
+                Main2Activity.this.data = result;
 
                 runOnUiThread(new Runnable() {
                     @Override
@@ -68,25 +78,24 @@ public class Main2Activity extends AppCompatActivity {
                     }
                 });
             }
-
-            private String[] parseJSONObject(String responseText) {
-                JSONArray jsonArray=null;
-                try{
-                     jsonArray=new JSONArray(responseText);
-                    String[] result=new String[jsonArray.length()];
-                    for (int i=0;i<jsonArray.length();i++){
-                        JSONObject jsonObject=jsonArray.getJSONObject(i);
-                        result[i]=jsonObject.getString("name");
-                    }
-                    return result;
-                }catch(JSONException e) {
-                   e.printStackTrace();
-                }
-               return null;
-            }
-
         });
-
-
     }
-}
+        private String[] parseJSONObject(String responseText) {
+            JSONArray jsonArray=null;
+            try{
+                jsonArray=new JSONArray(responseText);
+                String[] result=new String[jsonArray.length()];
+                for (int i=0;i<jsonArray.length();i++){
+                    JSONObject jsonObject=null;
+                    jsonObject=jsonArray.getJSONObject(i);
+                    this.data[i]=jsonObject.getString("name");
+                    this.pids[i]=jsonObject.getInt("id");
+                }
+                return result;
+            }catch(JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
